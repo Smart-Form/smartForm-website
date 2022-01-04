0c850282969de39f39a5d03085a1e2b096b47bda
Cyrus Lau <cyruslau.work@gmail.com>
2022-01-05 00:52:25 +0800

    enter

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//scroll
 $(document).ready(function(){ 
     $(window).scroll(function(){ 
         if ($(this).scrollTop() > 100) { 
@@ -12,3 +12,25 @@ $(document).ready(function(){
         return false; 
     }); 
 });
+
+//form-validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
